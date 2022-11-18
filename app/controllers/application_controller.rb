58d055142f3ca06b62fbc74ba94f1f9172b255be
William Gadd <wgadd@sasktel.net>
2022-11-18 01:35:10 +0000

    Add links on nav bar for logging in or out and registering and display username if logged in. Set /logout to redirect to session#delete instead of session#create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-
+  helper_method :current_user
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#create'
-  
+  get '/logout', to: 'sessions#destroy'
+
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
 
