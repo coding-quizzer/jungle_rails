0853f20d0a6c0b8fa0d27bcd20c12b8be0c7e18d
William Gadd <wgadd@sasktel.net>
2022-11-18 01:13:09 +0000

    Add logic to sessions_controller to create a new session after user has logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:email])
+      session[:user_id] = user.id
+      redirect_to '/'
+    end
   end
 
   def destroy
   end
-  
+
 end
