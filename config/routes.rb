2d8c9bc960cbddf6effe771b14cc2e8a13ffddad
William Gadd <wgadd@sasktel.net>
2022-11-18 00:24:25 +0000

    Add routes to login and logout sessions and and controllers to render login page

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,14 @@ Rails.application.routes.draw do
 
   get "/about", to: 'about#index'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#create'
+  
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
+
+
   
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
