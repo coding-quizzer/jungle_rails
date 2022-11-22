73e7bf23c06a74b81366ba75a7a3e0591a1bc8c2
William Gadd <wgadd@sasktel.net>
2022-11-22 02:16:59 +0000

    Set up tests to ensure that first_name, last_name, and password are required for user registration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,14 +14,29 @@ RSpec.describe User, type: :model do
     
     it 'should not allow users to register with an already existing email' do
       User.create!(first_name: "Robert", last_name: "Smith", email: "robby@gmail.com", password: "whereami", password: "whereami")
-      @new_user = User.create(first_name: "Bob", email: "robby@gmail.com", password: "howisit", password_confirmation: "howisit")
+      @new_user = User.create(first_name: "Bob", last_name: "Richardson", email: "robby@gmail.com", password: "howisit", password_confirmation: "howisit")
       expect(@new_user.errors.full_messages).to include "Email has already been taken"
     end
 
     it 'should not allow users to register with an already existing email' do
-      User.create!(first_name: "Robert", last_name: "Smith", email: "RoBBy@gmail.com", password: "whereami", password: "whereami")
-      @new_user = User.create(first_name: "Bob", email: "robby@gmail.com", password: "howisit", password_confirmation: "howisit")
+      User.create!(first_name: "Robert", last_name: "Smith", email: "RoBBy@gmail.com", password: "whereami", password_confirmation: "whereami")
+      @new_user = User.create(first_name: "Bob", last_name: "Richardson", email: "robby@gmail.com", password: "howisit", password_confirmation: "howisit")
       expect(@new_user.errors.full_messages).to include "Email has already been taken"
+    end
+
+    it 'should not allow users to register without a valid first name' do
+      @user = User.create(last_name: "Smith", email: "robby@gmail.com", password: "whereami", password_confirmation: "whereami")
+      expect(@user.errors.full_messages).to include "First name is required"
+    end
+
+    it 'should not allow users to register without a valid last name' do
+      @user = User.create(first_name: "Bob", email: "robby@gmail.com", password: "whereami", password_confirmation: "whereami")
+      expect(@user.errors.full_messages).to include "Last name is required"
+    end
+
+    it 'should not allow users to register without a valid last name' do
+      @user = User.create(first_name: "Bob", last_name: "Smith", password: "whereami", password_confirmation: "whereami")
+      expect(@user.errors.full_messages).to include "Email is required"
     end
   end
 
