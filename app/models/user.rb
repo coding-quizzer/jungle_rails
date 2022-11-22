de7f2e2de7e3e76fc6e5da629cb1ef4fd74c66ed
William Gadd <wgadd@sasktel.net>
2022-11-22 02:24:27 +0000

    Add validations in users model, ensuring the the user's first name, last name and email are all present

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: { case_sensitive: false}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
   has_secure_password
 
 end
