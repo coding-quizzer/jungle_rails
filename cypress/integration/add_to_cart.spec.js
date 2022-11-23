946addff9b8f00efe5000ffc94557d41d2117c3e
William Gadd <wgadd@sasktel.net>
2022-11-23 00:42:00 +0000

    Test that if user clicks add to cart button, the cart count increases by 1

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+describe ('jungle add product to cart', () => {
+  it('User can add a product to cart and the cart will contain gain an item', () => {
+  cy.visit('/');
+  cy.contains('My Cart (0)')
+  cy.contains('Scented Blade')
+    .scrollIntoView()
+    .get('.products article')
+    .contains('Add')
+    .click({scrollBehavior: false});
+
+  cy.contains('My Cart (1)')
+
+  });
+});
