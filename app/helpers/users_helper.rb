c2e7adca908dca7846a468e66a5fa78c1b304a08
William Gadd <wgadd@sasktel.net>
2022-11-23 23:38:10 +0000

    Move helpers for identifying current user into the users_helper file

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+  def current_user
+   User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_full_name
+    current_user && "#{current_user.first_name} #{current_user.last_name}" 
+  end
+
+  def displayed_user_name
+    user_full_name || "Khurram Virani"
+  end
+
+  def displayed_user_email
+    (current_user && current_user.email) || 'kvirani@gmail.com'
+  end
+
+
 end
